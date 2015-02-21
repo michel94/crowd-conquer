7a83f384dd9f49c381c3585a614bb14d82dddf5d
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-02-21 04:17:54 +0000

    Added start conquer api function

--- web/server/api.js
+++ web/server/api.js
@@ -1,10 +1,47 @@
+Router.onBeforeAction(Iron.Router.bodyParser.urlencoded({
+        extended: false
+    }));
 
-
-Router.route('/restful', {where: 'server'})
+/*Router.route('/api/example/', {where: 'server'})
   .get(function () {
+  	console.log(this.params);
     this.response.end('get request\n');
   })
   .post(function () {
-    this.response.end('post request\n');
-  });
+  	console.log(this.params);
+  	console.log(this.request);
+  	console.log(this.body);
+  	console.log(this.data);
+
+  	var json = this.request.body
+  	console.log(json);
+
+  	this.response.writeHead(200, {'Content-Type': 'application/json'});
+    this.response.end(JSON.stringify({asd: 'asd'}) );
+  });*/
+
+function response(post, message){
+	post.response.writeHead(400, {'Content-Type': 'application/json'});
+    post.response.end(JSON.stringify({'message': message}) );
+}
+
+Router.route('/api/start', {where: 'server'})
+	.post(function(){
+		if(!this.request.body.args){
+			response(this, 'No args parameter found');
+			return;
+		}
+
+		var json = this.request.body.args;
+		try{
+			json = JSON.parse(json);
+		}catch(err){
+			response(this, 'Json could not be parsed');
+			return;
+		}
+
+		this.response.writeHead(200, {'Content-Type': 'application/json'});
+    	this.response.end(JSON.stringify({}) );
+		
+	});
 
