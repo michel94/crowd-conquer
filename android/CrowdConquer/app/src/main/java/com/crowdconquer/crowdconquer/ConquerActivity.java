c4a70903dcba9345805862efb2cfb3561ce0b4cf
josemarcelino <jose.marcelino@jeknowledge.com>
2015-02-21 16:06:08 +0000

    gps and wifi validation android, isGPSEnabled? isWIFIEnabled?, alert

--- android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/ConquerActivity.java
+++ android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/ConquerActivity.java
@@ -1,8 +1,13 @@
 package com.crowdconquer.crowdconquer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -19,12 +24,75 @@ public class ConquerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_conquer);
 
-        startLocationService();
-        initViews();
-        new Thread(updateProgressBar).start();
+        boolean isGPSEnabled;
+        boolean isNetworkEnabled;
+        boolean canIStart = true;
+
+        Context mContext = this;
+
+        LocationManager locationManager = (LocationManager) mContext
+                .getSystemService(LOCATION_SERVICE);
+
+        isGPSEnabled =locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworkEnabled =locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (isNetworkEnabled == false || isGPSEnabled == false) {
+
+              showSettings(this);
+            }
+
+        isGPSEnabled =locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworkEnabled =locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if(isNetworkEnabled == false && isGPSEnabled == false) {
+            finish();
+        }
+        else {
+            setContentView(R.layout.activity_conquer);
+
+            startLocationService();
+            initViews();
+            new Thread(updateProgressBar).start();
+        }
     }
+
+
+    private void showSettings(final Context mContext) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS or WIFI is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+
+    }
+
 
     private void initViews() {
         conquerProgressBar = (ProgressBar)findViewById(R.id.circularProgressbar);
