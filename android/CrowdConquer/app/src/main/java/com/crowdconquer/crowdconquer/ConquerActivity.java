44248e2f0eac747f18bb37f9be1aac4fa6c57ca5
ABaptista <amccb@student.dei.uc.pt>
2015-02-21 02:40:10 +0000

    Services added. Not tested.

---
+++ android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/ConquerActivity.java
@@ -0,0 +1,22 @@
+package com.crowdconquer.crowdconquer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.crowdconquer.crowdconquer.services.LocationService;
+
+public class ConquerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conquer);
+        startLocationService();
+    }
+
+    private void startLocationService() {
+        Intent intent = new Intent(this, LocationService.class);
+        startService(intent);
+    }
+}
