789c846bd015986f4c322062675c122c5c01e0b9
ABaptista <amccb@student.dei.uc.pt>
2015-02-21 05:50:07 +0000

    HTTP request added.

--- android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/data/StaticData.java
+++ android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/data/StaticData.java
@@ -2,4 +2,5 @@ package com.crowdconquer.crowdconquer.data;
 
 public class StaticData {
     public static User user;
+    public static String SERVER_1 = "http://10.6.0.136:3000/api/";
 }
---
+++ android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/services/Api.java
@@ -0,0 +1,10 @@
+package com.crowdconquer.crowdconquer.services;
+
+import com.crowdconquer.crowdconquer.utils.Request;
+
+public class Api {
+    public static void startConquer() {
+        Request request = new Request("post", "start");
+        request.execute();
+    }
+}
