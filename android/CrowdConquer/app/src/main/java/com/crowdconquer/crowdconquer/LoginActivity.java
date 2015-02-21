71efb7431a1c1a8a08c697e79ba0ca17f7b7edba
ABaptista <amccb@student.dei.uc.pt>
2015-02-21 04:08:32 +0000

    Data package added.

--- android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/LoginActivity.java
+++ android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/LoginActivity.java
@@ -10,9 +10,6 @@ public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
---
+++ android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/data/User.java
@@ -0,0 +1,17 @@
+package com.crowdconquer.crowdconquer.data;
+
+public class User {
+    private String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
