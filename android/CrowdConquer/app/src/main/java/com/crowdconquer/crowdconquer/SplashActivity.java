8feb8f61a4ca6489b6f390ce02a6dec7b1832880
ABaptista <amccb@student.dei.uc.pt>
2015-02-21 15:39:51 +0000

    Splash screen added.

--- android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/SplashActivity.java
+++ android/CrowdConquer/app/src/main/java/com/crowdconquer/crowdconquer/SplashActivity.java
@@ -5,28 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends Activity {
+public class SplashActivity extends Activity {
     //views
-    Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_splash);
         initViews();
         startListeners();
     }
 
     private void initViews() {
-        buttonSignIn = (Button) findViewById(R.id.btn_sign_in);
+
     }
 
     private void startListeners() {
-        buttonSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
     }
 }
