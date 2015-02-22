5296e21e002fd8fece521c001354e4f22efde17f
Guilherme Graca <ggracase@gmail.com>
2015-02-22 14:00:36 +0000

    Merged

--- web/client/client.js
+++ web/client/client.js
@@ -11,7 +11,7 @@ Tracker.autorun(function() {
     if (Meteor.user()){
         email = Meteor.user().services.google.email;
 
-        Meteor.call("login", email);
+        Meteor.call("myLogin", email);
         //console.log(email);
         //if(!Users.findOne({email: email}))
         //    Users.insert({email: email});
