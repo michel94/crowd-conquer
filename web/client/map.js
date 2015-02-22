ddc212436a46740dfa1cdb915dd7909f3f49feda
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-02-22 04:10:29 +0000

    BAPTISTA'S BUGS

--- web/client/map.js
+++ web/client/map.js
@@ -25,7 +25,87 @@ function drawCell(cell){
 }
 
 function initMap() {
-    if(Session.get("initialized")){
+    
+}
+
+Template.map.rendered = function(){
+    Session.set("initialized", true);
+}
+
+Template.map.helpers({
+    dummy: function(){
+        console.log(Cells.find().fetch());
+
+        if(Session.get("initialized")){
+            var mapOptions = {
+                zoom: 15,
+                center: new google.maps.LatLng(40.186, -8.416),
+                mapTypeId: google.maps.MapTypeId.ROAD,
+                backgroundColor: "#141414"
+            };
+
+            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+            var styles = [{
+                "stylers": [
+                      { "invert_lightness": true },
+                      { "hue": "#00a1ff" },
+                      { "saturation": 30 }
+                    ]
+                }
+                ,{
+                    "featureType": "road",
+                    "elementType": "labels",
+                    "stylers": [{ "visibility": "off" }]
+                }
+                ,{
+                    "featureType": "road.highway",
+                    "stylers": [
+                      { },
+                      { "color": "#090909"},
+                      { "saturation": 0 }
+                    ]
+                  }
+                 ,{
+                    "featureType": "water",
+                    "stylers": [
+                      { "color": "#0099FF" } ,
+                      { "saturation": -60 }
+                    ]
+                  }
+                 ,{
+                    "featureType": "landscape",
+                    "stylers": [
+                      { "saturation": -90 },
+                      { "hue": "#0099FF" }
+                    ]
+                  }];
+
+            map.setOptions({styles: styles});
+
+            console.log(Cells.find().fetch());
+
+            var cells = Cells.find();
+            cells.forEach(function(cell){
+                console.log("cell");
+                drawCell(cell);
+            });
+            //drawCell({lat: 40.186, lon: -8.416}, map);
+            //drawCell(40.187, -8.416, map);
+            //drawCell(40.186, -8.417);
+        }
+    }
+});
+
+Template.welcome.rendered = function(){
+    Session.set("initialized", true);
+}
+
+Template.welcome.helpers({
+    dummy: function(){
+        console.log(Cells.find().fetch());
+
+        if(Session.get("initialized")){
         var mapOptions = {
             zoom: 15,
             center: new google.maps.LatLng(40.186, -8.416),
@@ -83,24 +163,5 @@ function initMap() {
         //drawCell(40.187, -8.416, map);
         //drawCell(40.186, -8.417);
     }
-}
-
-Template.map.rendered = function(){
-    Session.set("initialized", true);
-}
-
-Template.map.helpers({
-    dummy: function(){
-        initMap();
-    }
-});
-
-Template.welcome.rendered = function(){
-    Session.set("initialized", true);
-}
-
-Template.welcome.helpers({
-    dummy: function(){
-        initMap();
     }
 });
