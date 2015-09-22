9e6d1d7b2c454d8f820a9f880164a0a474aa5d2f
josemarcelino <jose.m.s.marcelino@gmail.com>
2015-09-23 00:22:00 +0100

    added android base

---
+++ CrowdConquer-Android/app/src/main/java/com/example/crowdconquer/crowdconquer_android/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.crowdconquer.crowdconquer_android;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
