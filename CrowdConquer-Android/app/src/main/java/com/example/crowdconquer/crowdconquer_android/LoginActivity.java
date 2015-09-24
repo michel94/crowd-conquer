f8a574525adb7d9f677863b96ba2addfa2b5fb41
josemarcelino <jose.m.s.marcelino@gmail.com>
2015-09-24 02:41:33 +0100

    Float Action Button lib, Main View with webView

--- CrowdConquer-Android/app/src/main/java/com/example/crowdconquer/crowdconquer_android/LoginActivity.java
+++ CrowdConquer-Android/app/src/main/java/com/example/crowdconquer/crowdconquer_android/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.example.crowdconquer.crowdconquer_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import com.rey.material.widget.ProgressView;
+import com.rey.material.widget.FloatingActionButton;
+
 
 
 public class LoginActivity extends Activity {
 
-    ProgressView loadingLogin;
+    FloatingActionButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,25 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //loadingLogin = (ProgressView) findViewById(R.id.progressLoginId);
-        //loadingLogin.start();
+        loginButton = (FloatingActionButton) findViewById(R.id.fabId);
+
+        setOnClickListenerLogin();
+
+
+
     }
+
+
+    void setOnClickListenerLogin(){
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
 }
---
+++ CrowdConquer-Android/app/src/main/java/com/example/crowdconquer/crowdconquer_android/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.crowdconquer.crowdconquer_android;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class MainActivity extends Activity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webView = (WebView) findViewById(R.id.webViewId);
+
+        webView.loadUrl("http://crowdconquer.meteor.com/");
+    }
+
+
+
+}
