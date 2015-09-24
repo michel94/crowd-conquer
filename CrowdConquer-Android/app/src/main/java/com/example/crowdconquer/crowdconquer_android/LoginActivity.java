07ddb51b5bfa8a72c86e3995cc4b45b5c5927021
josemarcelino <jose.m.s.marcelino@gmail.com>
2015-09-24 02:55:36 +0100

    Location validation on Login

--- CrowdConquer-Android/app/src/main/java/com/example/crowdconquer/crowdconquer_android/LoginActivity.java
+++ CrowdConquer-Android/app/src/main/java/com/example/crowdconquer/crowdconquer_android/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.crowdconquer.crowdconquer_android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 
 import com.rey.material.widget.FloatingActionButton;
@@ -33,11 +38,36 @@ public class LoginActivity extends Activity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                if(isAppReadyToLogin()) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
 
             }
         });
+    }
+
+    boolean isAppReadyToLogin(){
+
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) || !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location Services Not Active");
+            builder.setMessage("Please enable Location Services and GPS and try again");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            Dialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+            return false;
+        }else{
+            return true;
+        }
+
     }
 
 
