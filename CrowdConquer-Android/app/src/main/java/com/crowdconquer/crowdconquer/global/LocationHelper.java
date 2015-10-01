9821f8613f402a7ae11e79b33b34897a0f8b0ac3
josemarcelino <jose.m.s.marcelino@gmail.com>
2015-10-01 23:02:11 +0100

    Gps Location Service

--- CrowdConquer-Android/app/src/main/java/com/crowdconquer/crowdconquer/global/LocationHelper.java
+++ CrowdConquer-Android/app/src/main/java/com/crowdconquer/crowdconquer/global/LocationHelper.java
@@ -5,16 +5,41 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 
 
 public class LocationHelper {
 
     LocationManager locationManager;
+    Location actualLocation;
+    LocationListener locationListener;
+    private static final int ONE_MINUTE = 1000 * 60;
 
     public LocationHelper(LocationManager locationManager){
         this.locationManager = locationManager;
+
+        this.locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                Log.e("GPS Location", actualLocation + "");
+                setActualLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        this.locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
     public boolean checkLocationManagerStatus(Context context){
@@ -40,5 +65,69 @@ public class LocationHelper {
         alertDialog.setCanceledOnTouchOutside(false);
         alertDialog.show();
     }
+
+    public Location getActualLocation() {
+        return actualLocation;
+    }
+
+    public void setActualLocation(Location actualLocation) {
+        if(isBetterLocation(actualLocation, this.actualLocation))
+            this.actualLocation = actualLocation;
+    }
+
+
+
+
+    boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > ONE_MINUTE;
+        boolean isSignificantlyOlder = timeDelta < -ONE_MINUTE;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more one minute since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than one minute older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+
+    boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
+
+
 
 }
