02e5f18872c959a7d2e191fc258bfa03fa696d24
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2015-12-13 20:30:11 +0000

    Clean up main server file

--- CrowdConquer-Server/server/server.js
+++ CrowdConquer-Server/server/server.js
@@ -1,61 +1,9 @@
 Fiber = Meteor.npmRequire('fibers');
 
 Meteor.startup(function(){
-	wsServer = new WSServer(8080);
+	setupWebsocket();
 
-	function onRequest(request){
-		// TODO verify request.origin
 
-		var connection = request.accept(null, request.origin);
-		console.log(new Date() + 'New connection');
-
-		connection.on('message', function(message) {
-			if (message.type === 'utf8') {
-				console.log('Received Message: ' + message.utf8Data);
-				//connection.send(message.utf8Data);
-
-				if(RPC.enabled){
-					result = RPC.parseData(message.utf8Data)
-					console.log(result) //TODO IMPORTANT Check if result is error code or Object
-
-					Fiber(function(){
-						if(!result.hasOwnProperty('callbackId') )
-							Meteor.apply(result.methodName, result.args);
-						else{
-							Meteor.apply(result.methodName, result.args, function(error, response){
-								ret = JSON.stringify({
-									protocol: 'rpcCallback',
-									rpcCallback: {
-										error: error,
-										response: response,
-										callbackId: result.callbackId
-									}
-								})
-								console.log('send callback ' + ret);
-								connection.send(ret)
-							});
-						}
-					}).run();
-				}
-
-			}else if (message.type === 'binary') {
-				console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-				connection.sendBytes(message.binaryData);
-			}
-
-		});
-
-		connection.on('close', function(reasonCode, description) {
-			console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-		});
-	}
-
-	function onError(e){
-		console.log('Error', e);
-	}
-
-	wsServer.open(onRequest, onError)
-	
 
 	Meteor.methods({
 		hello: function(){
@@ -65,8 +13,5 @@ Meteor.startup(function(){
 		}
 	});
 
-	//d = {a:0, b:1, c:0};
-	//console.log(d.contains(['a', 'b']));
-	//console.log(d.contains(['a', 'e']));
 });
 
